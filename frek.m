ddb45f7f5b1abce16b989cd27aa64d56f917181f
blgylmz61 <blgylmz61@icloud.com>
2015-12-14 23:30:54 +0200

    frek fonksiyonu yazıldı ve frek.m dsyası oluşturuldu

--- frek.m
+++ frek.m
@@ -1,4 +1,4 @@
 function frekans = frek(nota,oktav)  % oktavý verilen notanýn frekansýný döndüren fonksiyon
 notalar=['do','d1','re','mb','mi','fa','f1','sol','s1','la','lb' ,'si']; %notalar bir diziye atandý.
 n=findstr(notalar,nota); % fonksiyona gönderilen nota dizi içerisinde aratýldý ve indeksi döndürüldü.
-frekans=16,35*(2^oktav)*(2^n/12); % notanýn frekansý matematiksel olarak hesaplandý
+frekans=16.35*(2^oktav)*(2^n/12); % notanýn frekansý matematiksel olarak hesaplandý
