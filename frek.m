d5037e077a8dec212e73c5c03571e09ac8d52acc
Trfaxa <oznurr_yilmazz@hotmail.com>
2015-12-15 13:16:20 +0200

    frek.m dosyası güncellendi

--- frek.m
+++ frek.m
@@ -1,4 +1,5 @@
-function frekans = frek(nota,oktav)  % oktavý verilen notanýn frekansýný döndüren fonksiyon
-notalar=['do','d1','re','mb','mi','fa','f1','sol','s1','la','lb' ,'si']; %notalar bir diziye atandý.
-n=findstr(notalar,nota); % fonksiyona gönderilen nota dizi içerisinde aratýldý ve indeksi döndürüldü.
-frekans=16.35*(2^oktav)*(2^n/12); % notanýn frekansý matematiksel olarak hesaplandý
+function [F]=frek(n,o)%frekans döndürmektedir.
+nota={'do','c#','re','eb','mi','fa','f#','sol','g#','la','bb','si'}; %notolardan oluþan bir dizi yazdýk.
+n=strcmp(nota,n); %Dizenin içine aktarý,indekside 1 artýrýr.
+m=find(n,1);%n dizisinde 1 deðerinin indeksini döndürür.
+F=16.35*(2^o)*(2^((m-1)/12));%frekans fonksiyonu oluþturuldu.
